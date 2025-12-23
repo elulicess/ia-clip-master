d2d568a3e3c9719b1a8a96dbe36a26951db61110
elulicess <iaprotm@gmail.com>
2025-12-23 01:23:22 -0600

    Fix formatting and whitespace in processor.py

--- processor.py
+++ processor.py
@@ -1,13 +1,14 @@
 import numpy as np
+from moviepy import VideoFileClip
 
 def detectar_momentos_clave(video_path):
-    clip = VideoFileClip(video_path)
+    clip = VideoFileClip(video_path) 
+    
     audio = clip.audio
-    fps = 44100 
+    fps = 44100
     audio_frames = audio.to_soundarray(fps=fps)
     
     volumen = np.sqrt(np.mean(audio_frames**2, axis=1))
-    
     
     segundo_maximo = np.argmax(volumen) / fps
     
